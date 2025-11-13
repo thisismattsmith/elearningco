d868461ef92643007639f0831514e7a15d6a68f2
thisismattsmith <hello@thisismattsmith.com>
2025-11-13 16:04:27 +1100

    Sitemap creation
    Install astro sitemap and updated config files. Also set up robots.txt creation through robots.txt.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+    site: 'https://elearningcompany.net',
+  integrations: [sitemap()],
+});
 // https://astro.build/config
-export default defineConfig({});
+
+
